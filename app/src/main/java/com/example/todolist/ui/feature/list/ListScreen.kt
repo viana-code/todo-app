187219d6a8bf232e75763b139af51ef7cf65fe6c
João Guilherme Araújo Viana <joaoguilhermearaujo1617@gmail.com>
2025-07-24 20:16:49 -0300

    :lipstick: Adição de TopBar.

--- app/src/main/java/com/example/todolist/ui/feature/list/ListScreen.kt
+++ app/src/main/java/com/example/todolist/ui/feature/list/ListScreen.kt
@@ -7,9 +7,12 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -75,12 +78,20 @@ fun ListScreen(
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ListContent(
     todos: List<Todo>,
     onEvent: (ListEvent) -> Unit
     ) {
     Scaffold (
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = "Todo List")
+                }
+            )
+        },
         floatingActionButton = {
             FloatingActionButton(onClick = {
                 onEvent(ListEvent.AddEdit(null))
